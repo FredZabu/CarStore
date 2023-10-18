f8c806af74b8f0dd29fed6289a5961f5dc84f923
Fred Zabulon <fredzabulon133@gmail.com>
2023-10-18 21:37:38 +0300

    added in the total value functionality

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,11 +1,20 @@
 import './CarValue.css';
-
+import { useSelector } from 'react-redux';
 function CarValue() {
+
+    const totalCost = useSelector((state) => {
+        const { cars, searchTerm } = state.cars;
+        return cars.filter((car) => {
+            return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+        }).reduce((acc, car) => {
+          return acc + car.cost
+        }, 0)
+    })
   return (
       <div className="carValue-container">
           <div>
               <h3>Total Value</h3>
-              <p>$3000</p>
+              <p>${totalCost}</p>
           </div>
     </div>
   )
