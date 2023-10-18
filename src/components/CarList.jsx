224dafc00a3a62858fe4b4a5812e7ebdb5f8119f
Fred Zabulon <fredzabulon133@gmail.com>
2023-10-18 22:04:11 +0300

    added in the partial match functionality

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -3,17 +3,21 @@ import { removeCar } from '../store/index.js';
 import './CarList.css'
 function CarList() {
     const dispatch = useDispatch();
-    const carlist = useSelector((state) => {
+    const {carlist, name} = useSelector((state) => {
         const { cars, searchTerm } = state.cars;
-        return cars.filter((car) => {
+        const { name } = state.form;
+        return {
+            carlist:cars.filter((car) => {
             return car.name.toLowerCase().includes(searchTerm.toLowerCase());
-        })
+        }),
+        name: name
+        }
     })
   return (
       <div>
           {carlist.map((car) => {
               return <div className="list-container" key={car.id}>
-                  <p> {car.name} - ${car.cost}  </p>
+                  <p className={` ${name && car.name.toLowerCase().includes(name.toLowerCase())? 'changer': 'tochanger'}`}> {car.name} - ${car.cost}  </p>
                   <button onClick={() => {
                       dispatch(removeCar(car.id))
                   }}>Delete</button>
